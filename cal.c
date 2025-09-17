ec2e0fda4e2ab6d49546dd81884668ccdd6c2dac
Keshika-20 <murthykeshika@gmail.com>
2025-09-17 17:46:09 +0530

    fork2

--- cal.c
+++ cal.c
@@ -3,11 +3,19 @@ int add(int a,int b)
 {
     return a+b;
 }
+
+int sub(int b,int a)
+{
+
+    return b-a;
+}
 int main()
 
 {
     int a=2,b=5;
     int c=add(a,b);
+    int d=sub(b,a);
     printf("%d",c);
+    printf("%d",d);
 
 }
